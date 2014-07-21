f64d7be8e46be17d552206ffa6e3384b79745552
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2014-07-21 17:19:50 +0200

    Added marlin steering xml file

--- pylcio/pylcio-test.py
+++ pylcio/pylcio-test.py
@@ -7,9 +7,20 @@ from pyLCIO import EVENT
 import sys
  
 # create a reader and open an LCIO file
-reader = IOIMPL.LCFactory.getInstance().createLCReader()
 
-reader.open( "pythiaZPolebbbar_with_particle_tbl.10.full.slcio"  )
+
+def print_params(event, inputCollectionTypeName=None, inputCollectionName=None):
+    for collectionName, collection in event:
+        if ((not inputCollectionName) or collectionName == inputCollectionName) \
+                and ((not inputCollectionTypeName) or collection.getTypeName() == inputCollectionTypeName):
+            print "CN: " + str(collectionName) + ", CTN: " + str(collection.getTypeName())
+            params =  collection.getParameters()
+            for intKey in params.getIntKeys():
+                print intKey + " : " + params.getIntVal(intKey)
+            for floatKey in params.getFloatKeys():
+                print floatKey + " : " + params.getFloatVal(floatKey)
+            for stringKey in params.getStringKeys():
+                print stringKey + " : " + params.getStringVal(stringKey)
 
 def walk_particles(root, depth=0):
     tabs = ""
@@ -20,17 +31,31 @@ def walk_particles(root, depth=0):
     for daughter in root.getParticles():
         walk_particles(daughter, depth+1)
 
+
+if __name__ == "__main__":
+    reader = IOIMPL.LCFactory.getInstance().createLCReader()
+    if len(sys.argv) == 2:
+        print "Opening argv[1] = " + sys.argv[1]
+        reader.open( sys.argv[1] )
+    else:
+        print "Opening default = pythiaZPolebbbar_with_particle_tbl.10.full.slcio" 
+        reader.open( "pythiaZPolebbbar_with_particle_tbl.10.full.slcio"  )
+
+    for event in reader:
+        # inputCollectionTypeName="ReconstructedParticle"
+        print_params(event)
+
 # loop over all events in the file
+"""
 for event in reader:
     print 'Content of event number %s' % ( event.getEventNumber() )
 
-    """
+
     BuildUpVertex = event.getCollection("BuildUpVertex")
 
     for particle in BuildUpVertex:
         print particle.getEnergy()
-    """
-    """
+
     primary_vertex = None
     # loop over all the collections in the event
     for collectionName, collection in event:
@@ -42,8 +67,7 @@ for event in reader:
                 
     primary_particle = primary_vertex.getAssociatedParticle()
     walk_particles(primary_particle)
-    """
-    
+
     for collectionName, collection in event:
         if collection.getTypeName() == "ReconstructedParticle":
             for recon_particle in collection:
@@ -56,8 +80,7 @@ for event in reader:
                     for stringKey in params.getStringKeys():
                         print stringKey
                     break
-                    
-    """
+
     reco_particle_collection = event.getCollection("PandoraPFOCollection")
     mc_particle_collection =  event.getCollection("MCParticle")
 
@@ -66,8 +89,7 @@ for event in reader:
     for particle in mc_particle_collection:
         if particle.getPDG() == 5 or particle.getPDG() == -5: 
             print "Mass: " + str(particle.getMass()) + " Charge: " + str(particle.getCharge()) + " Type: " + str(particle.getPDG()) + "Momentum: (" + str(particle.getMomentum()[0]) + ", " + str(particle.getMomentum()[1]) + ", "+ str(particle.getMomentum()[2]) + ")" 
-    """
-    """
+
     for collectionName, collection in event:
         if collection.getTypeName() == "Vertex":
         #for thing in collection:
@@ -92,6 +114,7 @@ for event in reader:
                 print key
                 if params.getNString(key) == 1:
                     print params.getStringVal(key) 
-    """
+   
 
 reader.close()
+"""
