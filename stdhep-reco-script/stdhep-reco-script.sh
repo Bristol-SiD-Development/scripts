0785668bc3c05b8caa1eba2ac797ecbc13ab1af1
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2014-07-24 11:53:01 +0200

    Updated stdhep-reco-script.sh to be able to run LCFIPlus flavour tagging

--- stdhep-reco-script/stdhep-reco-script.sh
+++ stdhep-reco-script/stdhep-reco-script.sh
@@ -18,13 +18,15 @@ pandora_binary=$ilcsoft_root_dir/slicPandora/v01-00-00/build/bin/PandoraFrontend
 marlin_binary=$ilcsoft_root_dir/Marlin/v01-05/bin/Marlin
 anajob_binary=$ilcsoft_root_dir/lcio/v02-04-03/bin/anajob
 
+
 echo "Running slic simulation..."
+#/cern.ch/user/o/oreardon/public/ilc/simulation \
 
 $slic_binary -g /afs/cern.ch/user/o/oreardon/public/ilc/simulation/detectors/sidloi3/sidloi3.lcdd \
--i /afs/cern.ch/user/o/oreardon/public/ilc/pythiaZPolebbbar.stdhep \
--o /cern.ch/user/o/oreardon/public/ilc/simulation/pythiaZPolebbbar_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3.slcio \
--p /afs #/cern.ch/user/o/oreardon/public/ilc/simulation \
--r 10 \
+-i /afs/cern.ch/user/o/oreardon/public/ilc/pythiaZPoleccbar-0-1000.stdhep \
+-o pythiaZPoleccbar-0-1000_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3.slcio \
+-p /afs/cern.ch/user/o/oreardon/public/ilc/simulation \
+-r 30 \
 -P /afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6/v01-17-05/slic/v03-01-03/simulation/particle.tbl
 
 #$slic_binary /afs/cern.ch/user/o/oreardon/public/ilc/macro/stdhep.macro
@@ -32,9 +34,8 @@ $slic_binary -g /afs/cern.ch/user/o/oreardon/public/ilc/simulation/detectors/sid
 if [ "$?" -ne "0" ]; then
   exit 1
 fi
-
                    
-input="/afs/cern.ch/user/o/oreardon/public/ilc/simulation/pythiaZPolebbbar_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3.slcio"
+input="/afs/cern.ch/user/o/oreardon/public/ilc/simulation/pythiaZPoleccbar-0-1000_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3.slcio"
 input_no_ext=${input%.*}
 
 echo $input_no_ext
@@ -52,11 +53,11 @@ if [ "$?" -ne "0" ]; then
 fi
 
 
-#We'll need to restore the old version so marlin works
+#Well need to restore the old version so marlin works
 old_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ilcsoft_root_dir/slicPandora/v01-00-00/build/lib/
 
-pandora_output="/afs/cern.ch/user/o/oreardon/public/ilc/simulation/pythiaZPolebbbar_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_pandora.slcio"
+pandora_output="/afs/cern.ch/user/o/oreardon/public/ilc/simulation/pythiaZPoleccbar-0-1000_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_pandora.slcio"
 
 echo "Runing pandora..."
 $pandora_binary -g /afs/cern.ch/user/o/oreardon/public/ilc/myscripts/sidloi3_pandora.xml -c /afs/cern.ch/user/o/oreardon/public/ilc/myscripts/sid_dbd_pandoraSettings.xml -i $lcsim_1_output -o $pandora_output
@@ -69,31 +70,40 @@ fi
 #cp ../myscripts/sid_dbd_vertexing.xml .
 #emacs sid_dbd_vertexing.xml #make changes as shown at https://confluence.slac.stanford.edu/display/~stanitz/From+Zero+to+SiD+-+Running+Sim+Reco
 
-#Clean out the crap we've put in $LD_LIBRARY_PATH. Marlin complained about duplicate libraries.
+#Clean out the crap weve put in $LD_LIBRARY_PATH. Marlin complained about duplicate libraries.
 LD_LIBRARY_PATH=$old_LD_LIBRARY_PATH
 
+first_marlin_output="pythiaZPoleccbar-0-1000_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_lcfi.slcio"
+
 echo "Running marlin.."
-$marlin_binary /afs/cern.ch/user/o/oreardon/public/ilc/simulation/BB_sid_dbd_vertexing.xml 
-#$marlin_binary /afs/cern.ch/user/o/oreardon/public/ilc/data/stearingFiles/v02_p01/flavortag.xml
+#$marlin_binary /afs/cern.ch/user/o/oreardon/public/ilc/simulation/BB_sid_dbd_vertexing.xml 
+$marlin_binary --global.LCIOInputFiles=$pandora_output --MyLCIOOutputProcessor.LCIOOutputFile=$first_marlin_output /afs/cern.ch/user/o/oreardon/public/ilc/simulation/sid_dbd_vertexing.xml 
 if [ "$?" -ne "0" ]; then
   exit 1
 fi
 
-
-lcsim_2_input="pythiaZPolebbbar_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_lcfi.slcio"
-lcsim_2_recFile="pythiaZPolebbbar_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_full.slcio"
-lcsim_2_dstFile="pythiaZPolebbbar_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_dst.slcio"
+lcsim_2_recFile="pythiaZPoleccbar-0-1000_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_full.slcio"
+lcsim_2_dstFile="pythiaZPoleccbar-0-1000_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_dst.slcio"
 
 echo "Running second lcsim reco..."
-java -jar $lcsim_jar ../myscripts/sid_dbd_postPandora.xml -DinputFile=$lcsim_2_input -DrecFile=$lcsim_2_recFile -DdstFile=$lcsim_2_dstFile
+java -jar $lcsim_jar ../myscripts/sid_dbd_postPandora.xml -DinputFile=$first_marlin_output -DrecFile=$lcsim_2_recFile -DdstFile=$lcsim_2_dstFile
 
 
 if [ "$?" -ne "0" ]; then
   exit 1
 fi
 
-echo "Running anajob ..."
-$anajob_binary $lcsim_2_recFile
+
+#lcsim_2_dstFile="pythiaZPoleccbar-0-1000_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_dst.slcio"
+
+
+echo "Running marlin on dst..."
+#$marlin_binary --global.LCIOInputFiles=$lcsim_2_dstFile --MyLCIOOutputProcessor.LCIOOutputFile="secondMarlinOutputFromPfos.slcio" /afs/cern.ch/user/o/oreardon/public/ilc/data/stearingFiles/v02_p01/flavortagfrompfo.xml
+
+$marlin_binary --global.LCIOInputFiles=$lcsim_2_dstFile --MyLCIOOutputProcessor.LCIOOutputFile="pythiaZPoleccbar-0-1000_slic-3.1.3_geant4-v9r6p1_QGSP_BERT_sidloi3_dst_flavourtag.slcio" /afs/cern.ch/user/o/oreardon/public/ilc/data/stearingFiles/v02_p01/flavortag.xml
+
+#echo "Running anajob ..."
+#$anajob_binary $lcsim_2_recFile
 
 if [ "$?" -ne "0" ]; then
   exit 1
