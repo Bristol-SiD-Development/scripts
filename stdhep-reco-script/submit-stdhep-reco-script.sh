dd0a2c167acd07c608381750a85c85c773071312
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2014-07-29 17:03:24 +0200

    Updated reco lxbatch submission script

--- stdhep-reco-script/submit-stdhep-reco-script.sh
+++ stdhep-reco-script/submit-stdhep-reco-script.sh
@@ -9,4 +9,9 @@ ilcsoft_root_dir=$ilcsoft_root/$ilcsoft_version
 echo "Sourcing init_ilcsoft.sh..."
 source $ilcsoft_root_dir/init_ilcsoft.sh
 
-/afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/stdhep-reco-script.py -o /afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/output-2000/ -r 2000 -s /afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/steering_files/ -g /afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/geometry_files/ /afs/cern.ch/user/o/oreardon/public/ilc/pythiaZPolebbbar.stdhep 
+/afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/stdhep-reco-script.py \
+-o /afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/output_500_events_good/ \
+-r 500 \
+-s /afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/steering_files/ \
+-g /afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/geometry_files/ \
+/afs/cern.ch/user/o/oreardon/public/ilc/pythiaZPolebbbar.stdhep 
