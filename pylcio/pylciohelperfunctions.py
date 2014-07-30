55389b609a258827878f2cbe93cfc91e9d0251ed
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2014-07-30 15:54:12 +0200

    added helper function to get the flavour of the event jets from the mc data

--- pylcio/pylciohelperfunctions.py
+++ pylcio/pylciohelperfunctions.py
@@ -1,3 +1,7 @@
+from pyLCIO import IOIMPL
+from pyLCIO import UTIL
+from pyLCIO import EVENT
+
 import math
 
 def sumVectors(v1, v2):
@@ -50,3 +54,53 @@ def reconstructEventTheta(event):
 
 def reconstructEventEta(event):
     return -math.log(math.tan( reconstructEventTheta(event) / 2.))
+    
+
+def get_b_and_c_likenesses(event):
+    collection = event.getCollection("RefinedJets")
+    
+    pidh = UTIL.PIDHandler(collection)
+    algo = pidh.getAlgorithmID( "lcfiplus" )
+    ibtag = pidh.getParameterIndex(algo, "BTag")
+    ictag = pidh.getParameterIndex(algo, "CTag")
+
+    likenesses = []
+
+    for jet in collection: #should be two of them...
+        pid = pidh.getParticleID(jet, algo)
+        likenesses.append({"BTag": pid.getParameters()[ibtag], "CTag": pid.getParameters()[ictag]})
+        
+    return likenesses
+
+def decaysToQQBar(root):
+    daughters = root.getDaughters()
+
+    qPDGs = range(1, 7)
+    qBarPDGs = range(-6, 0)
+
+    qs = filter(lambda p: any([p.getPDG() == qPDG for qPDG in qPDGs]),daughters)
+    qBars = filter(lambda p: any([p.getPDG() == qPDG for qPDG in qBarPDGs]),daughters)
+
+    for element in itertools.product(qs, qBars):
+        if element[0].getPDG() == -element[1].getPDG():
+            #print "found qqBar: {0} {1}".format(element[0].getPDG(), element[1].getPDG())
+            return abs(element[0].getPDG())
+    return None
+
+def walkMcParticlesUntilQQBar(root, depth=0):
+    qPDG = decaysToQQBar(root)
+    if qPDG:
+        return qPDG
+
+    for daughter in root.getDaughters():
+        ret = walkMcParticlesUntilQQBar(daughter, depth+1)
+        if ret != -1:
+            return ret
+            
+    return -1
+
+def get_jet_flavor_from_mc(event):
+    for mcParticle in event.getCollection("MCParticlesSkimmed"):
+        if len(mcParticle.getParents()) == 0:
+            return walkMcParticlesUntilQQBar(mcParticle, 0)
+            break
