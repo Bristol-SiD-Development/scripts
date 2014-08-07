32a462c4d754f11af6f2b9525d03585f3afe0a43
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2014-08-07 12:02:54 +0200

    Added full support for parallel processing through the LXbatch system

--- parallel-reco-jobs/parallel-reco-jobs.py
+++ parallel-reco-jobs/parallel-reco-jobs.py
@@ -2,13 +2,15 @@
 
 from subprocess import check_call
 import argparse
-
+from datetime import datetime 
+import os
+import stat #so we can make the scripts executable
 def parse_args(steering_files, geometry_files):
     #Note that is impossible to set the weight and probability options that marlin takes because the resulting string would have multiple '.'s in it... 
     current_directory = os.getcwd()
     
-    default_steering_dir = os.path.join(current_directory, "steering_files")
-    default_geometry_dir = os.path.join(current_directory, "geometry_files")
+    default_steering_dir = os.path.join("/afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/", "steering_files")
+    default_geometry_dir = os.path.join("/afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/", "geometry_files")
 
     parser = argparse.ArgumentParser("Run the SiD reco chain")
 
@@ -28,9 +30,13 @@ def parse_args(steering_files, geometry_files):
     parser.add_argument("-r", "--runs",
                         help="Number of events to run per job",
                         default=10)
+    parser.add_argument("-q", "--batch-queue",
+                        help="Queue to submit to (see the bsub command)",
+                        default="8nh")
     parser.add_argument("-n", "--num-jobs",
                         help="Number of jobs to run",
-                        default=1)
+                        default=1,
+                        type=int)
 
     parser.add_argument("-d" ,"--delete-intermediate-files",
                         help="Deletes intermediate (all except the last) files as they become useless to save on disk usage. Defaults to False (as the intermediate files may be useful)",
@@ -71,12 +77,81 @@ def setup_binary_dict(ilcsoft_dir):
                 "anajob": anajob_binary}
     return binaries
 
-
 def main():
     steering_files = setup_steering_dict()
     geometry_files = setup_geom_dict()
-    args = get_args(steering_files, geometry_files)
 
+    args = parse_args(steering_files, geometry_files)
+
+    current_directory = os.getcwd()
+
+    #We'll make a timestamped directory to put all our (tempory) job submission scripts in
+    script_time_str = datetime.now().isoformat()
+    script_dir = os.path.join(current_directory, "job_scripts_{0}".format(script_time_str)) 
+    os.makedirs(script_dir)
+
+    job_indices = range(0, args.num_jobs)
+
+    #just set up a couple of lists of empty strings for now. We'll put the useful stuff in them in a bit
+    script_paths = [""]*args.num_jobs 
+    job_output_dirs = [""]*args.num_jobs
+    job_final_output_paths = [""]*args.num_jobs
+
+    #Put the useful stuff in them
+    for job_index in job_indices:
+        script_paths[job_index] = os.path.join(script_dir, "{0}.sh".format(job_index))
+        
+        job_output_dirs[job_index] = os.path.join(args.output_dir, "job_{0}".format(job_index))
+
+        input_dir, input_file_ext = os.path.split(args.stdhep_input)
+        input_file_no_ext, stdhep_ext = os.path.splitext(input_file_ext)
+        slcio_ext = ".slcio"
+        
+        job_final_output_paths[job_index] = os.path.join(job_output_dirs[job_index], input_file_no_ext + "_lcfiPlusFlavourTag" + slcio_ext)
+
+        os.makedirs(job_output_dirs[job_index])
+        
+        #I'm so sorry for this....
+        #We construct a varaiant of the submit-stdhep-reco-script.sh script
+        script_string ='#! /bin/bash \n\
+# Timestamp {0} \n\
+# Script number {1} \n\
+ilcsoft_version=v01-17-05 \n\
+ilcsoft_root=/afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6 \n\
+ilcsoft_root_dir=$ilcsoft_root/$ilcsoft_version \n\
+echo "Sourcing init_ilcsoft.sh..." \n\
+source $ilcsoft_root_dir/init_ilcsoft.sh \n\
+/afs/cern.ch/user/o/oreardon/public/ilc/scripts/stdhep-reco-script/stdhep-reco-script.py \\\n\
+-o {2}/ \\\n\
+-r {3} \\\n\
+-d \\\n\
+--skip-num {4} \\\n\
+-s {5} \\\n\
+-g {6} \\\n\
+--final-output-file {7} \\\n\
+{8}'.format(script_time_str, job_index, job_output_dirs[job_index], args.runs, job_index*int(args.runs), args.steering_dir, args.geometry_dir, job_final_output_paths[job_index],  args.stdhep_input)
+
+        #write it to disk
+        script_file = open(script_paths[job_index], "wb")
+        print >> script_file, script_string
+        script_file.close()
+
+        #make it executable, readable and writiable by everyone
+        os.chmod(script_paths[job_index],  stat.S_IRUSR ^ stat.S_IWUSR ^ stat.S_IXUSR  ^ stat.S_IROTH ^ stat.S_IWOTH ^ stat.S_IXOTH ^ stat.S_IRGRP ^ stat.S_IWGRP ^ stat.S_IXGRP)
+
+        #stick the script in the queue
+        check_call(["bsub",
+                    "-q", args.batch_queue, 
+                    script_paths[job_index],
+                    "-J", str(job_index)]) 
+        
+    lcfiPlus_files_file = open("output_files_{0}.txt".format(script_time_str),"w")
+    for job_final_output_path in job_final_output_paths:
+        print >> lcfiPlus_files_file, job_final_output_path
+    lcfiPlus_files_file.close()
     return 0
+
+
 if __name__ == "__main__":
     main()
+
