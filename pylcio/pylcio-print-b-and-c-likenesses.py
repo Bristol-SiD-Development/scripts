694b7356a6916f500b0f7a03cdbcd203a4ee5992
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2014-08-19 17:31:38 +0200

    Added code to find particles going round in circles

--- pylcio/pylcio-print-b-and-c-likenesses.py
+++ pylcio/pylcio-print-b-and-c-likenesses.py
@@ -6,7 +6,7 @@ from pyLCIO import EVENT
 from pylciohelperfunctions import *
 
 import sys
-
+import numpy as np
 
 def main():
     tag = None
@@ -20,15 +20,28 @@ def main():
         reader = IOIMPL.LCFactory.getInstance().createLCReader()
         reader.open( f )
     
-        for event in reader:
-            likenesses = get_b_and_c_likenesses(event)
-            actual_flavour = None
-            if tag:
-                actual_flavour = tag
-            else:
-                actual_flavour = get_decay_product_of_interesting_mcParticle(event)
-            for likeness_dict in likenesses:
-                print str(actual_flavour) + " " + " ".join([str(likeness_dict["BTag"]), str(likeness_dict["CTag"])])
+        for i, event in enumerate(reader):
+            #print >> sys.stderr,  i
+            try:
+                likenesses = get_b_and_c_likenesses(event)
+                actual_flavour = None
+                if tag:
+                    actual_flavour = tag
+                else:
+                    actual_flavour = get_decay_product_of_interesting_mcParticle(event)
+                for jet, likeness_dict in zip(event.getCollection("RefinedJets"), likenesses):
+                
+                    momentum = jet.getMomentum()[0], jet.getMomentum()[1], jet.getMomentum()[2]
+                
+                    tanTheta = np.sqrt(momentum[0]**2 + momentum[1]**2)/ momentum[2]
+                    theta = np.arctan(tanTheta)
+                    if 0.9 < theta < 2.3:
+                        location = 0
+                    else:
+                        location = 1
+                    print str(location) + " " + str(actual_flavour) + " " + " ".join([str(likeness_dict["BTag"]), str(likeness_dict["CTag"])])
+            except:
+                print >> sys.stderr, "Wat"
         reader.close()
 if __name__ == "__main__":
     main()
