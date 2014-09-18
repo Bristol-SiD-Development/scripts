5414d77bce9c734ab4127999de7ad18f013c325c
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2014-09-18 10:46:20 +0200

    Minor canges to allow parallel processing

--- pylcio/pylcioPrintTrackEfficienciesPuritiesTheta.py
+++ pylcio/pylcioPrintTrackEfficienciesPuritiesTheta.py
@@ -3,9 +3,17 @@ from pyLCIO import IOIMPL
 from pyLCIO import UTIL
 from pyLCIO import EVENT
 
-from pylciohelperfunctions import *
+#from pylciohelperfunctions import *
 
+from HelicalTrack import HelicalTrack
+from FastHashableObject import FastHashableHit, FastHashableMcp, FastHashableTrack
+from TrackAnalysis import TrackAnalysis
+
+from createRootNtuples import ManyToManyTable, ManyToOneTable, isLongLivedAndCharged, createHitToMcpTable, createTrackToMcpTable
+import numpy as np
 import sys
+import math
+
 
 def isLongLivedAndCharged(mcParticle):
     pdg =  mcParticle.getPDG() 
@@ -16,22 +24,31 @@ def isLongLivedAndCharged(mcParticle):
         return False
 
 def main():
-    tag = None
-    if len(sys.argv) < 2:
-        print >> stderr, "Error: script requires a slcio file as input!"
-        return -1
+    startEvent=int(sys.argv[1])
+    endEvent=int(sys.argv[2])
 
-    theta_bins = np.linspace(-0.1, np.pi + 0.1, 100)
-    good_track_bins = np.zeros_like(theta_bins)
-    fake_track_bins = np.zeros_like(theta_bins)
-    charged_mc_bins = np.zeros_like(theta_bins)
+    for inputFile in sys.argv[3:]:
+        lcioReader = IOIMPL.LCFactory.getInstance().createLCReader()
+        lcioReader.open(inputFile)
 
-    for f in sys.argv[1:]:
-        reader = IOIMPL.LCFactory.getInstance().createLCReader()
-        reader.open( f )
-        mcParticleCount = 0
-        for i, event in enumerate(reader):
-            print >> sys.stderr, i
+        theta_bins = np.linspace(0, np.pi, 100)
+        good_track_bins = np.zeros_like(theta_bins)
+        fake_track_bins = np.zeros_like(theta_bins)
+
+        trackCollectionName="Tracks"
+        mcParticleCollectionName="MCParticlesSkimmed"
+        hitMcpRelationCollectionName="HelicalTrackMCRelations"
+        trackMcpRelationCollectionName="TrackMCTruthLink"
+        lcioReader.skipNEvents(startEvent)
+        i  = startEvent
+        for event in lcioReader:
+            i += 1
+            if i > endEvent:
+                break
+            if not (startEvent < i < endEvent):
+                continue
+        
+            print >> sys.stderr, "Event: {0}".format(event.getEventNumber())
 
             foundTracks, foundMcParticles =  {}, {}
             track_to_associated_mc_particles = {}
@@ -42,7 +59,7 @@ def main():
 
             #Find all the trackerHits and set up lists to put their mcParticles in
             for trackerHit in event.getCollection("HelicalTrackHits"):
-                h_trackerHit = hashable_tracker_hit(trackerHit)
+                h_trackerHit = FastHashableHit(trackerHit)
                 trackerHit_to_mc_particles[h_trackerHit] = []
             
             #Loop over all the reations and append mcParticles to the list associated with their trackerHit
@@ -50,8 +67,8 @@ def main():
                 mcParticle = helicalTrackMCRelation.getTo()
                 trackerHit = helicalTrackMCRelation.getFrom()
 
-                h_trackerHit = hashable_tracker_hit(trackerHit)
-                h_mcParticle = hashable_mc_particle(mcParticle)
+                h_trackerHit = FastHashableHit(trackerHit)
+                h_mcParticle = FastHashableMcp(mcParticle)
 
                 trackerHit_to_mc_particles[h_trackerHit].append(h_mcParticle)
                 
@@ -64,13 +81,13 @@ def main():
 
             #Loop over all the tracks and set up lists to put their mcParticles in
             for track in event.getCollection("Tracks"):
-                h_recoTrack = hashable_reco_track(track)
+                h_recoTrack = FastHashableTrack(track)
                 track_to_associated_mc_particles[h_recoTrack] = []
 
             #Loop over all the mcParticle and set up lists to put their tracks in
             for mcParticle in event.getCollection("MCParticlesSkimmed"):
                 if isLongLivedAndCharged(mcParticle):
-                    h_mcParticle = hashable_mc_particle(mcParticle)
+                    h_mcParticle = FastHashableMcp(mcParticle)
                     mcParticle_to_associated_tracks[h_mcParticle] = []
 
             #Loop over all the track <-> mcParticle relations and fill the lists
@@ -78,8 +95,8 @@ def main():
                 recoTrack = trackMCTruthLink.getFrom()
                 mcParticle = trackMCTruthLink.getTo()
                 if isLongLivedAndCharged(mcParticle):
-                    h_recoTrack = hashable_reco_track(recoTrack)
-                    h_mcParticle = hashable_mc_particle(mcParticle)
+                    h_recoTrack = FastHashableTrack(recoTrack)
+                    h_mcParticle = FastHashableMcp(mcParticle)
                     
                     track_to_associated_mc_particles[h_recoTrack].append(h_mcParticle)
                     mcParticle_to_associated_tracks[h_mcParticle].append(h_recoTrack)
@@ -97,7 +114,8 @@ def main():
                 
                 badHits = False
                 #try:
-                associated_trackerHits = track.trackerHits
+
+                associated_trackerHits = map(FastHashableHit, track.getTrackerHits())
                 #firstHit = associated_trackerHits[0]
                 
                 if any( [ trackerHit_to_mc_particles[hit] == None for hit in associated_trackerHits ] ):
@@ -116,24 +134,24 @@ def main():
                 #    print >> sys.stderr, "Warning a track had no hits. Skipping..."
 
 
-            for track in good_tracks:
-                theta = track.getTheta()
+            for hTrack in good_tracks:
+                theta = math.atan2(1., hTrack.getTanLambda())
                 index = np.digitize([theta], theta_bins)
                 good_track_bins[index] += 1
 
-            for track in fake_tracks:
-                theta = track.getTheta()
+            for hTrack in fake_tracks:
+                theta = math.atan2(1., hTrack.getTanLambda())
                 index = np.digitize([theta], theta_bins)
                 fake_track_bins[index] += 1
 
-            for mcParticle in mcParticle_to_associated_tracks:
-                theta = mcParticle.getTheta()
-                index = np.digitize([theta], theta_bins)
-                charged_mc_bins[index] += 1
+            #for mcParticle in mcParticle_to_associated_tracks:
+            #    theta = math.atan2(1., HelicalTrack(inputMcp=mcParticle,bField=5.).tanL)
+            #    index = np.digitize([theta], theta_bins)
+            #    charged_mc_bins[index] += 1
                 
 
-    for theta_bin, good_track_bin, fake_track_bin, charged_mc_bin in zip(theta_bins, good_track_bins, fake_track_bins, charged_mc_bins):
-        print "{0} {1} {2} {3}".format(theta_bin, good_track_bin, fake_track_bin, charged_mc_bin)
+    for theta_bin, good_track_bin, fake_track_bin in zip(theta_bins, good_track_bins, fake_track_bins):
+        print "{0} {1} {2} {3}".format(theta_bin, good_track_bin, fake_track_bin,good_track_bin + fake_track_bin )
 
 
 if __name__ == "__main__":
